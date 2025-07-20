3b24de68ed0a0aa235b514a4b44fb0bc6bfad242
Adrian <annndriancito2012@gmail.com>
2025-07-20 16:46:03 +0200

    Yase se puede navegar en la web

--- IA_Meteorologica/web_app/django_app/weather_prediction/urls.py
+++ IA_Meteorologica/web_app/django_app/weather_prediction/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/', include('ml_trainer.urls')),
 ]
